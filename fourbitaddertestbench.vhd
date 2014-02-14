f9fa3d02668bc48dd065a7e5bd93ae32f9b7fe2f
TylerSpence <c16tyler.spence@usafa.edu>
2014-02-13 23:36:55 -0700

    Update fourbitaddertestbench.vhd

--- fourbitaddertestbench.vhd
+++ fourbitaddertestbench.vhd
@@ -1,14 +1,14 @@
 --------------------------------------------------------------------------------
--- Company: 
--- Engineer:
+-- Company: USAFA
+-- Engineer: Tyler Spence
 --
 -- Create Date:   20:51:04 02/11/2014
--- Design Name:   
+-- Design Name:   Final design
 -- Module Name:   C:/Users/C16Tyler.Spence/Desktop/Semester 4 classes/ece 281/fourbitadder/fourbitaddertestbench.vhd
 -- Project Name:  fourbitadder
--- Target Device:  
--- Tool versions:  
--- Description:   
+-- Target Device:  NEXYS2
+-- Tool versions:  n/a
+-- Description:   Testbench for a fourbit adder
 -- 
 -- VHDL Test Bench Created by ISE for module: fourbitadder
 -- 
@@ -46,7 +46,7 @@ sum : out std_logic_vector(3 downto
  
 signal a, b, sum : std_logic_vector(3 downto 0); 
 signal cout : std_logic; 
-signal incorrect : std_logic := „0?; 
+signal incorrect : std_logic := ï¿½0?; 
 signal input_vec: std_logic_vector(7 downto 0); 
  
 signal test_a, test_b, test_sum: std_logic_vector(4 downto 0); 
@@ -60,8 +60,8 @@ port map(a, b, cout, sum);
 a <= input_vec(3 downto 0); 
 b <= input_vec(7 downto 4); 
  
-test_a <= „0? & input_vec(3 downto 0); 
-test_b <= „0? & input_vec(7 downto 4); 
+test_a <= ï¿½0? & input_vec(3 downto 0); 
+test_b <= ï¿½0? & input_vec(7 downto 4); 
 test_sum <= test_a + test_b; 
  
 inputs : process 
@@ -73,7 +73,7 @@ for i in 0 to 255 loop
 wait for 50 ns; 
  
  if (cout /= test_sum(4)) or (sum /= test_sum(3 downto 0)) then 
-incorrect <= „1?; 
+incorrect <= ï¿½1?; 
  
 end if; 
  
